5243c9d7fbb1f94b2523eb013a7455eb83a2cdd9
tanishqsatsangi <tanishq.satsangi@pinelabs.com>
2025-07-10 00:13:24 +0530

    Version code to 2 and version name to 1.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.plural_pinelabs.native_express_sdk"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
