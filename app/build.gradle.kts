73520a2401d9e0e7ac799e9a915ac727037531ef
tanishqsatsangi <tanishq.satsangi@pinelabs.com>
2025-07-25 10:49:27 +0530

    Added convenience fees for the  Cards and UPI flow

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "com.plural_pinelabs.native_express_sdk"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
+        versionCode = 4
         versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
