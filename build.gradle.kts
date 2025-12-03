6c10505e01b44dc6a72d025497b08b8e89b50e61
tanishqsatsangi <tanishq.satsangi@pinelabs.com>
2025-12-03 16:25:10 +0530

    updated jitpack.yml 4

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,13 +5,4 @@ plugins {
     alias(libs.plugins.android.library) apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
     id("com.google.firebase.crashlytics") version "3.0.4" apply false
-   // `maven-publish`
-    //id("maven-publish") apply false   // good to declare it here too
-
-
-}
-
-
-tasks.register("publishToMavenLocal") {
-    dependsOn(":ExpressCheckoutSdk:publishMavenPublicationToMavenLocal")
 }
