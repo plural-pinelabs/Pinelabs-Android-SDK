feb9b7450ab628c15f894cf8999850d50f5ca309
PineLabs Online <plural-online@pinelabs.com>
2026-09-15 19:06:13 +0530

    removed last 9

--- expresscheckoutsdk/build.gradle.kts
+++ expresscheckoutsdk/build.gradle.kts
@@ -124,7 +124,6 @@ dependencies {
     implementation("com.facebook.shimmer:shimmer:0.5.0")
     implementation("io.coil-kt:coil:2.4.0")
     implementation("io.coil-kt:coil-svg:2.4.0")
-    implementation("io.last9:rum-android:1.1.7")
 
     // -------------------------------------------------
     // Coroutines
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,7 +16,6 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-        maven("https://cdn.last9.io/rum-sdk/android/maven/")
     }
 }
 
